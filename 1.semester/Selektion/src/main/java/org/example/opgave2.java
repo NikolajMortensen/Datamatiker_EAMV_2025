4fc02788ac2b9a2f59d1898d045af4bce54eb617
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-09-30 12:16:39 +0200

    exercises and solutions from docjava

---
+++ 1.semester/Selektion/src/main/java/org/example/opgave2.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class opgave2 {
+    public static void main(String[] args) {
+        int x = 41;
+        int y = 67;
+        int sum = x + y;
+
+        if (sum > 100) {
+            System.out.println("Summen er større end hundrede");
+        }
+
+    }
+
+
+
+}
