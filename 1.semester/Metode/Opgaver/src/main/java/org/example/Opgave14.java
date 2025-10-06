fa6e2a5cc32a8b6db414785b143a9440745e774b
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-10-06 21:30:13 +0200

    .

--- 1.semester/Metode/Opgaver/src/main/java/org/example/Opgave14.java
+++ 1.semester/Metode/Opgaver/src/main/java/org/example/Opgave14.java
@@ -2,24 +2,41 @@ package org.example;
 
 public class Opgave14 {
     
-    private static int add(int firstNumber,int secondNumber) {
-        return firstNumber + secondNumber;
+    private static int add(int x,int y) {
+        return x + y;
     }
     
-    private static int sub(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
+    private static int sub(int x, int y) {
+        return x - y;
     }
 
-    private static int mul(int firstNumber, int secondNumber) {
-        return firstNumber * secondNumber;
+    private static int mul(int x, int y) {
+        return x * y;
     }
 
-    private static int div(int firstNumber, int secondNumber) {
-        return firstNumber / secondNumber;
+    private static int div(int x, int y) {
+        return x / y;
     }
 
-    private static int mod(int firstNumber,int secondNumber) {
-        return firstNumber % secondNumber;
+    private static int mod(int x,int y) {
+        return x % y;
+    }
+
+
+    private static double power(double x, double y) {
+        return Math.pow(x, y);
+    }
+
+     private static double sqr(double x) {
+        return x * x;
+    }
+
+    private static double abs(double x) {
+        return Math.abs(x);
+    }
+
+    private static double sqrt(double x) {
+        return Math.sqrt(x);
     }
 
 
@@ -30,7 +47,11 @@ public class Opgave14 {
         System.out.println("B: " + sub(div(7, 3), mul(9, 5)));
         System.out.println("C: " + mul(4, add(3, 9)));
         System.out.println("D: " + add(div(9, 4), mod(8, 3)));
-        System.out.println("E: " + mod(add(div(9, 3), 9), add(3, 12)));
+        System.out.println("E: " + add(div(9, 3), add(mod(9, 3), 12)));
+        System.out.println( "power( 2, 3 ) = " + power( 2, 3 ) );
+        System.out.println( "sqr( 3 ) = " + sqr( 3 ) );
+        System.out.println( "abs( -5 ) = " + abs( -5 ) );
+        System.out.println( "sqrt( 41 ) = " + sqrt( 41 ) );
 
     }
 }
