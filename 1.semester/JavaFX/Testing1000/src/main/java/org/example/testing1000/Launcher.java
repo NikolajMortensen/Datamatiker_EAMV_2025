a817906eae8787afd6b1b4633b641bcd5b9a548e
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-10-21 12:17:10 +0200

    Added

---
+++ 1.semester/JavaFX/Testing1000/src/main/java/org/example/testing1000/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.testing1000;
+
+import javafx.application.Application;
+
+public class Launcher {
+    static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
