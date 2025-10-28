7523ebfb90ab90a37302bba36d9973ac07997aec
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-10-28 20:01:47 +0100

    Testet switching scenes

---
+++ 1.semester/JavaFX/SwitchScenes/src/main/java/org/example/switchscenes/SceneController.java
@@ -0,0 +1,35 @@
+package org.example.switchscenes;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+    private Parent root;
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene1.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Scene2.fxml"));
+        root = fxmlLoader.load();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
