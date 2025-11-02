14c766b5b10bd5c12a3b39d8e210c8e1da91989b
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-11-02 20:31:59 +0100

    Finished slider

---
+++ 1.semester/JavaFX/Slider/src/main/java/org/example/slider/HelloController.java
@@ -0,0 +1,34 @@
+package org.example.slider;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+    @FXML
+    private Label myLabel;
+
+    @FXML
+    private Slider mySlider;
+
+    int myTemperature;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mySlider.valueProperty().addListener(new ChangeListener<Number>() {
+
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                myTemperature = (int) mySlider.getValue();
+                myLabel.setText(Integer.toString(myTemperature) + "*C");
+            }
+        });
+    }
+}
