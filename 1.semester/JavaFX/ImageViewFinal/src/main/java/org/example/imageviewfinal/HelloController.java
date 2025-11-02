8b81c1dfec1e5cbb74474819b4e469eafa19f590
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-11-02 18:06:03 +0100

    Finished textview exercise

--- 1.semester/JavaFX/ImageViewFinal/src/main/java/org/example/imageviewfinal/HelloController.java
+++ 1.semester/JavaFX/ImageViewFinal/src/main/java/org/example/imageviewfinal/HelloController.java
@@ -1,14 +1,19 @@
 package org.example.imageviewfinal;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    ImageView myImageView;
+    Button myButton;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    Image myImage = new Image(getClass().getResourceAsStream("IMG_6934.jpeg"));
+
+    public void displayImage() {
+        myImageView.setImage(myImage);
     }
 }
