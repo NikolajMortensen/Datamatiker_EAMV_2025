0efa1a9eb6288eca54da0e20c70631508744202a
NikolajMortensen <nikolajmortensen13@gmail.com>
2025-11-03 10:47:31 +0100

    Updated repo

--- 1.semester/JavaFX/Testing1000/src/main/java/org/example/testing1000/Launcher.java
+++
@@ -1,9 +0,0 @@
-package org.example.testing1000;
-
-import javafx.application.Application;
-
-public class Launcher {
-    static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
